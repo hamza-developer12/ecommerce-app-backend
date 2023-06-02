792010bec6b32b65804be3f8459062b8980384aa
hamza-developer12 <130216298+hamza-developer12@users.noreply.github.com>
2023-06-02 12:27:35 +0500

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ import orderRouter from './routes/orderRoutes.js';
 dotenv.config();
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5173",
     credentials: true,
 }));
 app.use(express.json());
