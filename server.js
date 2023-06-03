87fb308f0108949a0391e85e452156013b599189
hamza-developer12 <130216298+hamza-developer12@users.noreply.github.com>
2023-06-03 14:51:03 +0500

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ const app = express();
 
 app.use(cors({
     origin: "https://ecommerce-app-12.netlify.app",
-     methods: ['GET', 'PUT', 'POST', 'DELETE'],
     credentials: true,
 }));
 app.use(express.json());
