e781fe53b5ec4836ecb4d757b359363ecd761776
hamza-developer12 <130216298+hamza-developer12@users.noreply.github.com>
2023-06-03 15:10:36 +0500

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -56,7 +56,7 @@ export const login = async (req, res) => {
             expires: expiryDate,
             httpOnly: true,
             path: '/',
-            sameSite: "lax",
+            sameSite: "none",
             })
         return res.status(200).json({
             success: true, user: {
